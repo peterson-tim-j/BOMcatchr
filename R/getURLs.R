3a5bcf23c90c421d3ae9fd1c07047f4dffb67c91
Tim Peterson <tim.peterson@monash.edu>
2025-12-09 17:52:53 +1100

    DEM url removed

--- R/getURLs.R
+++ R/getURLs.R
@@ -2,7 +2,7 @@
 #'
 #' \code{getURLs} get URLS to AWAP and Australian 9s DEM.
 #'
-#' This function returns a list of default URLs used to download the AWAP and DEM data.
+#' This function returns a list of default URLs used to download the meteorological data.
 #'
 #' @return
 #' A list variable of URLs as characters.
@@ -14,8 +14,7 @@
 #' @export
 getURLs <- function() {
 
-  URLs = list(DEM='https://datagovau.s3.amazonaws.com/bioregionalassessments/BA_ALL/ALL/DATA/Geography/Physiography/DEMGA_9second_v3/ebcf6ca2-513a-4ec7-9323-73508c5d7b93.zip',
-              precip = 'https://www.bom.gov.au/web03/ncc/www/awap/rainfall/totals/daily/grid/0.05/history/nat/',
+  URLs = list(precip = 'https://www.bom.gov.au/web03/ncc/www/awap/rainfall/totals/daily/grid/0.05/history/nat/',
               Tmin  =  'https://www.bom.gov.au/web03/ncc/www/awap/temperature/minave/daily/grid/0.05/history/nat/',
               Tmax =   'https://www.bom.gov.au/web03/ncc/www/awap/temperature/maxave/daily/grid/0.05/history/nat/',
               vprp =   'https://www.bom.gov.au/web03/ncc/www/awap/vprp/vprph15/daily/grid/0.05/history/nat/',
