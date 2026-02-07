9da1fcda969cc97f81719a6d5196b364f34a6cad
Tim Peterson <tim.peterson@monash.edu>
2026-02-07 18:14:23 +1100

    Updated for new make/extract fns

--- tests/testthat/test-makeNetCDF_file.R
+++ tests/testthat/test-makeNetCDF_file.R
@@ -15,50 +15,33 @@ test_that("netCDF grid can be created",
 
           # Set names for netCDF files (in the system temp. directory).
           ncdfFilename = tempfile(fileext = '.nc')
-          ncdfSolarFilename = tempfile(fileext = '.nc')
 
           # Build netCDF grids for all data but only over the defined time period.
           file.names = makeNetCDF_file(ncdfFilename=ncdfFilename,
-                                       ncdfSolarFilename=ncdfSolarFilename,
                                        updateFrom=startDate, updateTo=endDate)
         },
         message='Testing creaion of netCDF grids.'
       )
 
       # Test the files were created
-      expect_true(file.exists(ncdfFilename),'Testing creation of non-solar netCDF file for data from one week ago')
-      expect_true(file.exists(ncdfSolarFilename),'Testing creation of solar netCDF file for data from one week ago')
+      expect_true(file.exists(ncdfFilename),'Testing creation of netCDF file for data from one week ago')
 
       # test file size is reasonably large
-      expect_gt(file.info(ncdfFilename)$size,1E6,'Testing non-solar netCDF file size is > 1MB')
-      expect_gt(file.info(ncdfSolarFilename)$size,1E6,'Testing solar netCDF file size is > 1MB')
+      expect_gt(file.info(ncdfFilename)$size, 1E6,'Testing netCDF file size is > 1MB')
 
       # Test the netcdf files can be opened.
       expect_no_error(
         {
-          ncout <- ncdf4::nc_open(ncdfFilename, write=T)
-          ncout.solar <- ncdf4::nc_open(ncdfSolarFilename, write=T)
+          summary.df <- AWAPer::file.summary(ncdfFilename)
+
+          summary.df
         },
-        message='Testing opening of netCDF grids.'
+        message='Testing opening of netCDF grids and reading summary info.'
       )
 
-      # Test the netcDF files can be opened
-      expect_s3_class( ncout, 'ncdf4')
-      expect_s3_class( ncout.solar, 'ncdf4')
-
-      # check the dimension names
-      expect_true(ncdf4::ncatt_get(ncout, "time")$long_name == "time", "Testing time dimension exists in netCDF file")
-      expect_true(ncdf4::ncatt_get(ncout, "Lat")$long_name == "Lat", "Testing Lat dimension exists in netCDF file")
-      expect_true(ncdf4::ncatt_get(ncout, "Lat")$long_name == "Lat", "Testing Lat dimension exists in netCDF file")
-      expect_true(ncdf4::ncatt_get(ncout.solar, "time")$long_name == "time", "Testing time dimension exists in solar netCDF file")
-      expect_true(ncdf4::ncatt_get(ncout.solar, "Lat")$long_name == "Lat", "Testing Lat dimension exists in solar netCDF file")
-      expect_true(ncdf4::ncatt_get(ncout.solar, "Lat")$long_name == "Lat", "Testing Lat dimension exists in solar netCDF file")
-
-      # test the netCDF files have the expected dimensions.
-      expect_length(ncout$dim$Lat$vals, 691)
-      expect_length(ncout$dim$Long$vals, 886)
-      expect_length(ncout.solar$dim$Lat$vals, 679)
-      expect_length(ncout.solar$dim$Long$vals, 839)
+      # Test the summary.df is a data frame and dimensions
+      expect_true(is.data.frame(summary.df))
+      expect_shape(summary.df, dim = c(5, 7))
 
       # Update netcDF grids and expect no errors
       expect_no_error(
@@ -66,20 +49,21 @@ test_that("netCDF grid can be created",
           endDate = startDate
           startDate = Sys.Date()-11
           makeNetCDF_file(ncdfFilename=ncdfFilename,
-                          ncdfSolarFilename=ncdfSolarFilename,
                           updateFrom=startDate, updateTo=endDate)
         },
         message='Testing updating of netCDF grids by two days prior'
       )
 
-      # # Delete temp files and folder
-      # expect_no_error(
-      #   {
-      #     unlink(ncdfFilename)
-      #     unlink(ncdfSolarFilename)
-      #     unlink(fdir, recursive = TRUE)
-      #   },
-      #   message='Removing files created during test'
-      # )
+      # Test the netcdf files can be opened.
+      expect_no_error(
+        {
+          summary.df <- AWAPer::file.summary(ncdfFilename)
+        },
+        message='Testing opening of netCDF grids and reading summary info.'
+      )
+
+      # Test the summary.df is a data frame and dimensions
+      expect_true(is.data.frame(summary.df))
+      expect_shape(summary.df, dim = c(5, 7))
     }
 )
