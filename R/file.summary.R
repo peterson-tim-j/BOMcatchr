394af77a8a8fa707ad036c09052c72f40ce76385
Tim Peterson <tim.peterson@monash.edu>
2026-03-10 18:51:12 +1100

    Get time.step to allow monthly precip

--- R/file.summary.R
+++ R/file.summary.R
@@ -34,7 +34,8 @@ file.summary <- function(ncfile) {
                           var.string = character(),
                           from = as.Date(character()),
                           to = as.Date(character()),
-                          timestep = character(),
+                          time.step = character(),
+                          time.datum = character(),
                           units = character(),
                           ellipsoid.crs = character()
                           )
@@ -42,6 +43,9 @@ file.summary <- function(ncfile) {
   # Get number of variables in all groups
   for (igrp in grps$grps) {
     nvars.tmp = RNetCDF::file.inq.nc(igrp)$nvars
+
+    # Get group time starting point
+    time.datum = RNetCDF::att.get.nc(igrp,'Time',"units")
 
     # Loop through each var of each group and get variable names
     # ONLY of those with THREE dimensions. ie not those thatare just
@@ -52,7 +56,7 @@ file.summary <- function(ncfile) {
       if (var.tmp$ndims == 3) {
         date.from =  RNetCDF::att.get.nc(igrp, i, 'Start_date')
         date.to =  RNetCDF::att.get.nc(igrp, i, 'End_date')
-        timestep =  RNetCDF::att.get.nc(igrp, i, 'timestep')
+        time.step =  RNetCDF::att.get.nc(igrp, i, 'time.step')
         units =  RNetCDF::att.get.nc(igrp, i, 'units')
 
         date.from = as.Date(date.from, '%Y-%m-%d')
@@ -66,7 +70,8 @@ file.summary <- function(ncfile) {
                                       var.string = var.string,
                                       from = date.from,
                                       to = date.to,
-                                      timestep = timestep,
+                                      time.step = time.step,
+                                      time.datum = time.datum,
                                       units = units,
                                       ellipsoid.crs = ellipsoid.crs)
 
