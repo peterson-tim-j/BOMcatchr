50e3561536077f1c5f1b1a4515701eafa82c2e18
Tim Peterson <tim.peterson@monash.edu>
2026-03-15 11:58:51 +1100

    add vignette prebuild

--- vignettes/build_commands.R
+++ vignettes/build_commands.R
@@ -5,16 +5,21 @@
 path <- find.package("AWAPer")
 system(paste(shQuote(file.path(R.home("bin"), "R")),"CMD", "Rd2pdf", shQuote(path)))
 
-# Pre-build the vignettes to avoid their execution during package installation.
-# NOTE: Must manually move image files from vignettes/figure/ to vignettes/ after knit
-#       and remove figure/ from .Rmd
 library(knitr)
 library(rmarkdown)
+
+# Convert vignette *.Rnw source diles to a Knitr *.Rms
 knitr::knit("vignettes/AWAPer.Rnw", output = "vignettes/AWAPer.Rmd")
 knitr::knit("vignettes/A_Make_data_grids.Rnw", output = "vignettes/A_Make_data_grids.Rmd")
 knitr::knit("vignettes/B_Point_rainfall.Rnw", output = "vignettes/B_Point_rainfall.Rmd")
 knitr::knit("vignettes/C_Catchment_avg_ET_rainfall.Rnw", output = "vignettes/C_Catchment_avg_ET_rainfall.Rmd")
 knitr::knit("vignettes/D_Catchment_avg_ET_types.Rnw", output = "vignettes/D_Catchment_avg_ET_types.Rmd")
+
+# Pre-build the vignette *.Rmd files
+devtools::build_vignettes()
+
+# View vignettes
+devtools::load_all()
 browseVignettes("AWAPer")
 
 # devtools checks added
