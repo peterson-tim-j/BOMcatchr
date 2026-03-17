33a50a8fe77bb729746bbddaa79891698935f600
Tim Peterson <tim.peterson@monash.edu>
2026-03-17 12:40:28 +1100

    Fix error handling

--- R/download.ASCII.file.R
+++ R/download.ASCII.file.R
@@ -46,62 +46,72 @@ download.ASCII.file <- function (url.string, ivar.url.ext, ivar.file.ext, ivar.t
     fid <- file(des.file.name, "wb")
     writeBin(bin.data, fid)
     close(fid)
-  },error = function(cond) {return(TRUE)})
+    didFail = 0
+  },error = function(cond) {return(1)})
 
   # Unzip file
-  OS <- Sys.info()
-  OS <- OS[1]
-  if (OS=='Windows') {
+  if (didFail == 0) {
+    OS <- Sys.info()
+    OS <- OS[1]
+    if (OS=='Windows') {
       if (file.exists(des.file.name) && didFail==0) {
 
         # Get list of files in the downloaded zip file
         # From: https://stackoverflow.com/questions/55355466/7z-list-only-filenames
-        hasError = F
-        tryCatch( exp = {
-            zip.fnames = system(paste0('7z l -ba "',des.file.name),intern = T)
-            zip.fnames = grep("D....", zip.fnames, invert = TRUE, fixed = TRUE, value = TRUE)
-            zip.fnames = sub("^.{53}", "", zip.fnames)
-          },
-          error = function(e) {
-            hasError = T
-            }
+        didFail = 1
+        didFail = tryCatch( exp = {
+          zip.fnames = system(paste0('7z l -ba "',des.file.name),intern = T)
+          zip.fnames = grep("D....", zip.fnames, invert = TRUE, fixed = TRUE, value = TRUE)
+          zip.fnames = sub("^.{53}", "", zip.fnames)
+          didFail = 0
+        },
+        error = function(e) {
+          return(1)
+        }
         )
 
         # Unzip downloaded file
-        tryCatch( exp = {
-          exitMessage = system(paste0('7z e -aoa -bso0 "',des.file.name, '"', ' -o', workingFolder),
-                               intern = T)
-        },
-        error = function(e) {
-          hasError = T
+        if (didFail==0) {
+          hasError = tryCatch( exp = {
+            exitMessage = system(paste0('7z e -aoa -bso0 "',des.file.name, '"', ' -o', workingFolder),
+                                 intern = T)
+          },
+          error = function(e) {
+            return(1)
           }
-        )
+          )
+        }
 
-      if (hasError) {
-        message('------------------------------------------------------------------------------------')
-        message('The program "7z" is either not installed or cannot be found. If not installed then')
-        message('install it from https://www.7-zip.org/download.html .')
-        message('Once installed, do the following step:')
-        message('  1. Click "Search Windows", search "Edit environmental variables for your account" and click on it.')
-        message('  2. In the "User variables" window, select the "Path", and click "Edit..."')
-        message('  3. In the "Edit environmental variable" window, click "New".')
-        message('  4. Paste the path to the 7zip application folder, and click OK.')
-        message('  5. Restart Windows.')
-        message('  6. Open the "Command Prompt" and enter the command "7z".')
-        message('     If setup correctly, this should output details such as the version, descriptions of commands, etc.')
-        message('------------------------------------------------------------------------------------')
-        stop()
+
+        if (didFail==1) {
+          message('------------------------------------------------------------------------------------')
+          message('The program "7z" is either not installed or cannot be found. If not installed then')
+          message('install it from https://www.7-zip.org/download.html .')
+          message('Once installed, do the following step:')
+          message('  1. Click "Search Windows", search "Edit environmental variables for your account" and click on it.')
+          message('  2. In the "User variables" window, select the "Path", and click "Edit..."')
+          message('  3. In the "Edit environmental variable" window, click "New".')
+          message('  4. Paste the path to the 7zip application folder, and click OK.')
+          message('  5. Restart Windows.')
+          message('  6. Open the "Command Prompt" and enter the command "7z".')
+          message('     If setup correctly, this should output details such as the version, descriptions of commands, etc.')
+          message('------------------------------------------------------------------------------------')
+          stop()
+        }
+      }
+    } else {
+      if (file.exists(des.file.name) && didFail==0) {
+        # Get list of files in the downloaded zip file
+        zip.fnames = system(paste('znew -v ',des.file.name));
+
+        # Unzip downloaded file
+        system(paste('znew -f ',des.file.name));
       }
     }
-  } else {
-    if (file.exists(des.file.name) && didFail==0) {
-      # Get list of files in the downloaded zip file
-      zip.fnames = system(paste('znew -v ',des.file.name));
-
-      # Unzip downloaded file
-      system(paste('znew -f ',des.file.name));
-    }
   }
+
+  if (didFail==1)
+    return(list(file.name='', didFail=1))
 
   # Remove zip file
   file.remove(des.file.name)
