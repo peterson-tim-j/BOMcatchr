9f512db209785698cde42a95d4d811df739cfa32
Tim Peterson <tim.peterson@monash.edu>
2026-04-24 14:55:11 +1000

    Fix URLs, title more concise.

--- R/raingauge.R
+++ R/raingauge.R
@@ -1,7 +1,7 @@
-#' @title Rain gauge 63005 data for 2010.
+#' @title Example rain gauge data.
 #' @docType data
 #' @description Daily data for rain gauge 63005 from 2010. It used by a vignette to evaluate the extracted point data. The data is from Bathurst, NSW.
 #' @format data.frame of 8 columns and 365 rows.
-#' @source Bureau of Meteorology, Australia. See https://www.bom.gov.au/climate/data/index.shtml
+#' @source Bureau of Meteorology, Australia. See \url{https://www.bom.gov.au/climate/data/index.shtml}
 #' @usage data("raingauge")
 "raingauge"
