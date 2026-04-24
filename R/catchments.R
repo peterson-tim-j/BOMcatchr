07cf17c960e32b381e8141f6ccfef06425578745
Tim Peterson <tim.peterson@monash.edu>
2026-04-24 14:55:00 +1000

    Fix URLs, title more concise.

--- R/catchments.R
+++ R/catchments.R
@@ -1,7 +1,9 @@
-#' @title Example catchment boundary polygons.
+#' @title Example catchment boundaries.
 #' @docType data
-#' @description Two example catchment boundaries as a SpatialPolygonsDataFrame. The catchments are Creswick Creek (ID 407214, Vic., Australia, see http://www.bom.gov.au/water/hrs/#id=407214) and
-#' Bet Bet Creek (ID 407220, Vic., Australia, see http://www.bom.gov.au/water/hrs/#id=407220). The catchments can be used to extract catchment average climate data usng \code{extractCatchmentData}
+#' @description Two example catchment boundaries as a SpatialPolygonsDataFrame. The catchments are Creswick Creek (ID 407214, Vic., Australia, see \url{http://www.bom.gov.au/water/hrs/#id=407214}) and
+#' Bet Bet Creek (ID 407220, Vic., Australia, see \url{http://www.bom.gov.au/water/hrs/#id=407220}).
+#'
+#' The catchments can be used to extract catchment average climate data usng \code{extractCatchmentData}
 #' @format SpatialPolygonDataFrame
 #' @seealso
 #' \code{\link{extractCatchmentData}} for extracting catchment average climate data.
