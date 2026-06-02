ae441451245a00c0174936752fc21553e84dce98
Tim Peterson <tim.peterson@monash.edu>
2026-06-03 09:30:49 +1000

    package version checks added

---
+++ R/zzz.R
@@ -0,0 +1,21 @@
+.onLoad <- function(libname, pkgname) {
+  required_version <- "1.9"
+  current_version <- packageVersion("terra")
+
+  if (current_version < required_version) {
+    stop(sprintf(
+      "Package 'terra' >= %s is required, but %s is installed. Please update 'terra'.",
+      required_version, current_version
+    ), call. = FALSE)
+  }
+
+  required_version <- "1.14"
+  current_version <- packageVersion("Evapotranspiration")
+
+  if (current_version < required_version) {
+    stop(sprintf(
+      "Package 'Evapotranspiration' >= %s is required, but %s is installed. Please update 'Evapotranspiration'.",
+      required_version, current_version
+    ), call. = FALSE)
+  }
+}
