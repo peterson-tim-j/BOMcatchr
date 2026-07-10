4196ae12d3e6b90c2c0497803d17a8047f678727
Tim Peterson <tim.peterson@monash.edu>
2026-07-10 11:23:37 +1000

    Replaced with extract_locations()

--- data-raw/catchments.R
+++
@@ -1,9 +0,0 @@
-## code to prepare `catchments` dataset goes here
-library(terra)
-
-catchments=terra::vect("data-raw/catchments.shp")
-
-terra::writeVector(catchments, "inst/extdata/catchments.gpkg", overwrite = TRUE)
-
-#usethis::use_data(catchments, overwrite = TRUE)
-
