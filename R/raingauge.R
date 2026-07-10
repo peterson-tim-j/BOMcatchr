38dfbeaadc949dd9db13bb2876cea13adac065a2
Tim Peterson <tim.peterson@monash.edu>
2026-07-10 11:25:50 +1000

    Replaced with weather_*

--- R/raingauge.R
+++
@@ -1,7 +0,0 @@
-#' @title Example rain gauge data.
-#' @docType data
-#' @description Daily data for rain gauge 63005 from 2010. It used by a vignette to evaluate the extracted point data. The data is from Bathurst, NSW.
-#' @format data.frame of 8 columns and 365 rows.
-#' @source Bureau of Meteorology, Australia. See \url{https://www.bom.gov.au/climate/data/index.shtml}
-#' @usage data("raingauge")
-"raingauge"
