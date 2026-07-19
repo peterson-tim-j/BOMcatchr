1167cde991be3fc7cb872459247c0bb35336612c
Tim Peterson <tim.peterson@monash.edu>
2026-07-19 21:03:02 +1000

    Updated to state six gauges.

--- R/weather_gauge_loc.R
+++ R/weather_gauge_loc.R
@@ -1,9 +1,9 @@
 #' @title Example weather gauge coordinates.
 #' @docType data
-#' @description Coordinates of the four example weather gauge within Australia. All four gauges have been selected
+#' @description Coordinates of the six example weather gauge within Australia. All six gauges have been selected
 #' to be approximately at the centre of a grid cell to minimise error introduced by interpolation of the
 #' extracted grid data.
-#' @format data.frame of 3 columns and 4 rows.
+#' @format data.frame of 3 columns and 6 rows.
 #' @source Bureau of Meteorology, Australia. See \url{https://www.bom.gov.au/climate/data/index.shtml}
 #' @usage data("weather_gauge_loc")
 "weather_gauge_loc"
--- R/weather_gauge_obs.R
+++ R/weather_gauge_obs.R
@@ -1,10 +1,10 @@
 #' @title Example weather gauge observed data.
 #' @docType data
-#' @description Daily rain and maximum temperature observations (1/1/2010 - 31/12/2010) from four weather gauge within Australia.
-#' The data is used by a vignette to evaluate the extracted point data. All four gauges have been selected
+#' @description Daily rain and maximum temperature observations (1/1/2010 - 31/12/2010) from six weather gauge within Australia.
+#' The data is used by a vignette to evaluate the extracted point data. All six gauges have been selected
 #' to be approximately at the centre of a grid cell to minimise error introduced by interpolation of the
 #' extracted grid data.
-#' @format data.frame of 8 columns and 365 rows.
+#' @format data.frame of 4 columns and 365*6 rows.
 #' @source Bureau of Meteorology, Australia. See \url{https://www.bom.gov.au/climate/data/index.shtml}
 #' @usage data("weather_gauge_obs")
 "weather_gauge_obs"
