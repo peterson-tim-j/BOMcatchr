3212df2336dc16a39b7cdaddf94c78f85444a7b0
Tim Peterson <tim.peterson@monash.edu>
2026-07-28 09:02:57 +1000

    Add source data start date.

--- R/grid_sources.R
+++ R/grid_sources.R
@@ -10,6 +10,7 @@
 #'  \item{\code{label}: string description of the variable,}
 #'  \item{\code{units}: string for units of the variable.}
 #'  \item{\code{time.step} : string for the time step of the data. \code{days} or \code{months} are accepted.}
+#'  \item{\code{date.start} : string for the first date of the source data. Expected format is %Y-%m-%d.}
 #'  \item{\code{data.URL} : string of URL to the source gridded data.}
 #'  \item{\code{data.file.extension} : string for the file extension of the downloaded compressed source data.}
 #'  \item{\code{data.file.format} : string for file extension to the file required within the downloaded file.}
@@ -26,11 +27,20 @@
 #' @export
 grid_sources <- function() {
 
-  vars = c('tmax', 'tmin', 'precip', 'precip.RMSE', 'precip.monthly', 'vprp_9am',  'vprp_3pm', 'solarrad')
+  vars = c('tmax',
+           'tmin',
+           'precip',
+           'precip.RMSE',
+           'precip.monthly',
+           'vprp_9am',
+           'vprp_3pm',
+           'solarrad')
+
   nvars = length(vars)
   var.data = data.frame(label = rep('', nvars),
                         units  = rep('', nvars),
                         time.step = rep('', nvars),
+                        date.start = rep('', nvars),
                         data.URL = rep('', nvars),
                         data.file.extension = rep('', nvars),
                         data.file.format = rep('', nvars),
@@ -42,6 +52,7 @@ grid_sources <- function() {
   var.data['tmax',] <- c(  'Max daily temperature',
                            'deg_C',
                            'days',
+                           '1910-01-01',
                            'https://www.bom.gov.au/web03/ncc/www/awap/temperature/maxave/daily/grid/0.05/history/nat/',
                            'grid.Z',
                            'grid',
@@ -52,6 +63,7 @@ grid_sources <- function() {
   var.data['tmin',] <- c(  'Min daily temperature',
                            'deg_C',
                            'days',
+                           '1910-01-01',
                            'https://www.bom.gov.au/web03/ncc/www/awap/temperature/minave/daily/grid/0.05/history/nat/',
                            'grid.Z',
                            'grid',
@@ -62,6 +74,7 @@ grid_sources <- function() {
   var.data['precip',] <- c('Total daily precipitation',
                            'mm/day',
                            'days',
+                           '1900-01-01',
                            'https://www.bom.gov.au/web03/ncc/www/awap/rainfall/totals/daily/grid/0.05/history/nat/',
                            'grid.Z',
                            'grid',
@@ -72,6 +85,7 @@ grid_sources <- function() {
   var.data['precip.RMSE',] <- c('Root mean square error of daily precipitation estimate',
                            'mm/day',
                            'days',
+                           '1900-01-01',
                            'https://www.bom.gov.au/web03/ncc/www/awap/rainfall/rmse/daily/grid/0.05/history/nat/',
                            'grid.Z',
                            'grid',
@@ -82,6 +96,7 @@ grid_sources <- function() {
   var.data['precip.monthly',] <- c('Total monthly precipitation',
                            'mm/month',
                            'months',
+                           '1900-01-01',
                            'https://www.bom.gov.au/web03/ncc/www/agcd/rainfall/totals/month/grid/0.05/history/nat/',
                            'grid.zip',
                            'txt',
@@ -92,6 +107,7 @@ grid_sources <- function() {
   var.data['vprp_9am',] <- c(  '9am daily vapour pressure',
                            'hpa',
                            'days',
+                           '1971-01-01',
                            'https://www.bom.gov.au/web03/ncc/www/awap/vprp/vprph09/daily/grid/0.05/history/nat/',
                            'grid.Z',
                            'grid',
@@ -102,6 +118,7 @@ grid_sources <- function() {
   var.data['vprp_3pm',] <- c(  '3pm daily vapour pressure',
                            'hpa',
                            'days',
+                           '1971-01-01',
                            'https://www.bom.gov.au/web03/ncc/www/awap/vprp/vprph15/daily/grid/0.05/history/nat/',
                            'grid.Z',
                            'grid',
@@ -112,6 +129,7 @@ grid_sources <- function() {
   var.data['solarrad',] <- c(  'Total daily solar radiation',
                            'MJ/m^2',
                            'days',
+                           '1990-01-01',
                            'https://www.bom.gov.au/web03/ncc/www/awap/solar/solarave/daily/grid/0.05/history/nat/',
                            'grid.Z',
                            'grid',
